fbd2e8fe32eccf9ef85219b8404111a1a710077d
llowoll <42293062+llowoll@users.noreply.github.com>
2019-08-23 22:29:22 +0900

    luckyseven prj commit

---
+++ LuckySeven/Assets/Script/PlayerBulletMoving.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletMoving : MonoBehaviour
+{
+    private float bulletSpeed = 15f;  // 탄환의 속도(무기 선택에 따라 바뀌게 만들어야 합니다)
+    private float destroyTime = 5f;  // 탄환 유지 시간("")
+    private float damage = 1f;  // 탄환의 위력("")
+
+    private float direction; // 플레이어의 방향(탄환 발사 방향)
+
+    private Vector2 bulletVelocity = new Vector2(0, 0);
+
+    private Rigidbody2D rigid;
+
+    private GameObject player;
+    private Animator animator;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        animator = player.GetComponent<Animator>();
+
+        rigid = GetComponent<Rigidbody2D>();
+
+        direction = animator.GetFloat("Dir");
+
+        if (direction == 1f)
+        {
+            bulletVelocity = new Vector2(bulletSpeed, 0);
+        }
+        else if(direction == -1f)
+        {
+            bulletVelocity = new Vector2(-bulletSpeed, 0);
+        }
+
+        rigid.AddForce(bulletVelocity, ForceMode2D.Impulse);
+
+        Destroy(gameObject, destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ LuckySeven/Assets/Script/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;  //발사할 탄환 
+    public float waitTerm = 1f;  //발사 주기
+    float shootTimer = 0; //시간을 잴 타이머
+    bool canShoot = true;
+
+    IEnumerator ShootBullet()
+    {
+        while (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+
+            while (shootTimer < waitTerm)
+            {
+                shootTimer += Time.deltaTime;
+                yield return new WaitForSeconds(0.01f);
+            }
+            shootTimer = 0;
+        }
+        canShoot = true;
+    }
+
+    void Update()
+    {
+        if (canShoot)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                canShoot = false;
+                StartCoroutine(ShootBullet());
+            }
+        }
+    }
+}
